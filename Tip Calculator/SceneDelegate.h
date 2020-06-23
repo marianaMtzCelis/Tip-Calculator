476774b3c4babc90dd1ee73c0373dde937e0c2d2
Mariana Martinez <marianamtzcelis@fb.com>
2020-06-23 15:46:27 -0500

    Initial Commit

---
+++ Tip Calculator/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Tip Calculator
+//
+//  Created by Mariana Martinez on 23/06/20.
+//  Copyright © 2020 Mariana Martinez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Tip Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calculator
+//
+//  Created by Mariana Martinez on 23/06/20.
+//  Copyright © 2020 Mariana Martinez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
